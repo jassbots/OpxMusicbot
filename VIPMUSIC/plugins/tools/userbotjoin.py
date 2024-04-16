9cb61de3a086546910aaf9aa59c66e51122f426e
JASS BOTS <157136102+jassbots@users.noreply.github.com>
2024-04-16 15:50:40 +0530

    Update userbotjoin.py

--- VIPMUSIC/plugins/tools/userbotjoin.py
+++ VIPMUSIC/plugins/tools/userbotjoin.py
@@ -130,7 +130,7 @@ async def leave_all(client, message):
     try:
         userbot = await get_assistant(message.chat.id)
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1001733534088:
+            if dialog.chat.id == -1002139661881:
                 continue
             try:
                 await userbot.leave_chat(dialog.chat.id)
