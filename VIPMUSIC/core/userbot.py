f7a8d65e1964fda572f2884009c617ce72d164d2
JASS BOTS <157136102+jassbots@users.noreply.github.com>
2024-04-16 10:31:57 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -62,11 +62,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
-                await self.one.join_chat("dhhdshhss6")
+                await self.one.join_chat("punjabiii_chat")
+                await self.one.join_chat("punjabiii_chat")
+                await self.one.join_chat("punjabiii_chat")
+                await self.one.join_chat("punjabiii_chat")
+                await self.one.join_chat("punjabiii_chat")
             except:
                 pass
             assistants.append(1)
@@ -94,10 +94,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("THE_VIP_BOY")
-                await self.two.join_chat("THE_VIP_BOY_OP")
-                await self.two.join_chat("TG_FRIENDSS")
-                await self.two.join_chat("VIP_CREATORS")
+                await self.two.join_chat("punjabiii_chat")
+                await self.two.join_chat("punjabiii_chat")
+                await self.two.join_chat("punjabiii_chat")
+                await self.two.join_chat("punjabiii_chat")
             except:
                 pass
             assistants.append(2)
@@ -117,10 +117,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("THE_VIP_BOY")
-                await self.three.join_chat("THE_VIP_BOY_OP")
-                await self.three.join_chat("TG_FRIENDSS")
-                await self.three.join_chat("VIP_CREATORS")
+                await self.three.join_chat("punjabiii_chat")
+                await self.three.join_chat("punjabiii_chat")
+                await self.three.join_chat("punjabiii_chat")
+                await self.three.join_chat("punjabiii_chat")
             except:
                 pass
             assistants.append(3)
@@ -140,10 +140,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("THE_VIP_BOY")
-                await self.four.join_chat("THE_VIP_BOY_OP")
-                await self.four.join_chat("TG_FRIENDSS")
-                await self.four.join_chat("VIP_CREATORS")
+                await self.four.join_chat("punjabiii_chat")
+                await self.four.join_chat("punjabiii_chat")
+                await self.four.join_chat("punjabiii_chat")
+                await self.four.join_chat("punjabiii_chat")
             except:
                 pass
             assistants.append(4)
@@ -163,10 +163,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("THE_VIP_BOY")
-                await self.five.join_chat("THE_VIP_BOY_OP")
-                await self.five.join_chat("TG_FRIENDSS")
-                await self.five.join_chat("dhhdshhss6")
+                await self.five.join_chat("punjabiii_chat")
+                await self.five.join_chat("punjabiii_chat")
+                await self.five.join_chat("punjabiii_chat")
+                await self.five.join_chat("punjabiii_chat")
             except:
                 pass
             assistants.append(5)
