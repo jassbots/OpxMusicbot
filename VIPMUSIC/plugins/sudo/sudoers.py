75ec81019777ff56ead8a7135d6764525e77172b
JASS BOTS <157136102+jassbots@users.noreply.github.com>
2024-05-22 07:11:14 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -59,7 +59,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝐍𝐢𝐤𝐚𝐥 𝐑𝐚𝐧𝐝𝐢 𝐁𝐚𝐥𝐚 𝐒𝐮𝐝𝐨𝐥𝐢𝐬𝐭 𝐃𝐞𝐤𝐡𝐧𝐞 𝐀𝐚𝐲𝐚 𝐇𝐚𝐢 𝐛𝐚𝐝𝐚🖕😎😂", show_alert=True)
+        return await callback_query.answer("ʀᴇ ᴘᴇɴ ᴅᴇʏᴀ ᴠᴇᴇʀᴀ..", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
